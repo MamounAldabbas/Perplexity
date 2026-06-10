d861b4eac00771dde6c937a104f6ae1a5ed8b377
Mamoun Aldabbas <mdbbs32@gmail.com>
2026-06-10 16:17:43 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("ctaBtn");
 
+  button.addEventListener("click", () => {
+    button.textContent = "Thanks for clicking!";
+  });
+});
